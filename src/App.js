c594e390b28038a3c2c55cde87e9f9a74679e74a
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2019-06-07 09:23:26 +0100

    create a UrlForm component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import UrlForm from "./components/UrlForm";
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <UrlForm />
     </div>
   );
 };
---
+++ src/components/UrlForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UrlForm = () => {
+  return (
+    <form>
+      <input />
+      <button>Analyse</button>
+    </form>
+  );
+};
+
+export default UrlForm;
