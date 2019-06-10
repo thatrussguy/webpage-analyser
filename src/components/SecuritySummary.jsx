6b3ed442b908b8f476c8a298d8fc87eaf189a28a
thatrussguy <1921615+thatrussguy@users.noreply.github.com>
2019-06-10 14:43:01 +0100

    check ssl certificate

--- src/components/SecuritySummary.jsx
+++ src/components/SecuritySummary.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Error from "../components/Error";
+
+import checkSsl from "../page-functions/checkSsl";
 
 const UniqueDomains = ({ url }) => {
+  const [error, setError] = useState(null);
+  const [sslDetails, setSslDetails] = useState(null);
+
+  useEffect(() => {
+    let mounted = true;
+
+    const fetchData = async () => {
+      mounted && setError(null);
+      const { sslDetails, error } = await checkSsl(url).catch(({ message }) => {
+        mounted && setError(message);
+        mounted && setSslDetails(null);
+      });
+      mounted && setSslDetails(sslDetails);
+      mounted && setError(error);
+    };
+    fetchData();
+
+    return () => {
+      mounted = false;
+    };
+  }, [url]);
+
   return (
     <div>
-      <h3>Security Summary:</h3>
-      <p>Not finished yet!</p>
+      {sslDetails && (
+        <div>
+          <h3>Security Summary:</h3>
+          <p>
+            Is a valid SSL certificate available for {url}?{" "}
+            {sslDetails.valid ? "Yes" : "No"}
+          </p>
+          <p>Valid from: {sslDetails.valid_from}</p>
+          <p>Valid to: {sslDetails.valid_to}</p>
+          <p>Days remaining until expiration: {sslDetails.days_remaining}</p>
+        </div>
+      )}
+      {error && <Error error={error} />}
     </div>
   );
 };
